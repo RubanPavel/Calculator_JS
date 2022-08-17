1cd6eaff4bfeb1e18de8e87ec930d3d7c4360e7f
Ruban Pavel <ruban.pavel@outlook.com>
2022-08-17 13:06:32 +0300

    new function

--- script.js
+++ script.js
@@ -3,47 +3,34 @@ var buttonPlus = document.getElementById("buttonPlus");
 var buttonMultiply = document.getElementById("buttonMultiply");
 var buttonDivide = document.getElementById("buttonDivide");
 
+var inputOne = document.getElementById("number1");
+var inputTwo = document.getElementById("number2");
+
+function getNumber1() {
+  return (number1 = Number(inputOne.value));
+}
+
+function getNumber2() {
+  return (number2 = Number(inputTwo.value));
+}
+
 function onButtonMinusClick() {
-  var inputOne = document.getElementById("number1");
-  var inputTwo = document.getElementById("number2");
-
-  var number1 = inputOne.value;
-  var number2 = inputTwo.value;
-
-  var minus = number1 - number2;
+  var minus = getNumber1 - getNumber2;
   window.alert(minus);
 }
 
 function onButtonPlusClick() {
-  var inputOne = document.getElementById("number1");
-  var inputTwo = document.getElementById("number2");
-
-  var number1 = Number(inputOne.value);
-  var number2 = Number(inputTwo.value);
-
-  var summ = number1 + number2;
+  var summ = getNumber1 + getNumber2;
   window.alert(summ);
 }
 
 function onButtonMultiplyClick() {
-  var inputOne = document.getElementById("number1");
-  var inputTwo = document.getElementById("number2");
-
-  var number1 = inputOne.value;
-  var number2 = inputTwo.value;
-
-  var multiply = Number(number1 * number2);
+  var multiply = getNumber1 * getNumber2;
   window.alert(multiply);
 }
 
 function onButtonDivideClick() {
-  var inputOne = document.getElementById("number1");
-  var inputTwo = document.getElementById("number2");
-
-  var number1 = inputOne.value;
-  var number2 = inputTwo.value;
-
-  var divide = Number(number1 / number2);
+  var divide = getNumber1 / getNumber2;
   window.alert(divide);
 }
 
